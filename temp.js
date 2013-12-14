02ab1f44422e25bb0156c81622bd6ecc21ef798c
Sam Saccone <samccone@gmail.com>
2013-12-14 17:53:07 -0500

    initial

---
+++ temp.js
@@ -0,0 +1,32 @@
+var five  = require('johnny-five');
+var net   = require('net');
+var _     = require('underscore');
+var clients = [];
+
+var emiter  = function(v){_.each(clients, function(c) {
+  try {
+    c.write(v + "\n")
+  } catch (e) {}})
+};
+
+five.Board().on('ready', function() {
+  var sensor = new five.Sensor("A0");
+
+  sensor.on("data", function() {
+    var voltage = this.value * 0.004882814;
+    var celsius = (voltage - 0.5) * 100;
+    var fahrenheit = celsius * (9 / 5) + 32;
+
+    emiter(~~fahrenheit + " F");
+  });
+});
+
+net.createServer(function(socket) {
+  console.log("-- new connecton --");
+  clients.push(socket);
+  socket.write("-- welcome to sam temp -- \n");
+  socket.on('end', function() {
+    clients.splice(clients.indexOf(socket), 1);
+    socket.destroy();
+  });
+}).listen(3000);
